2834ec75c22ce3ac3bc0eab0fb3a9e7a61c16e47
Júlio Werner <juliowernermm@gmail.com>
2021-12-01 08:22:16 -0300

    🚀 New projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,7 +7,13 @@ export interface ProjectsProps {
 }
 
 import RDFBanner from "public/projects/RDF-LP.png";
-import RDFLogo from "public/projects/RDF-Logo.png";
+import RDFLogo from "public/projects/RDF-logo.png";
+
+import AnimationsBanner from "public/projects/animations-banner.png";
+import AnimationsLogo from "public/projects/animations-logo.jpg";
+
+import InboxIcon from "public/projects/inbox-icon.png";
+import InboxBanner from "public/projects/inbox-banner.png";
 
 export const projects: ProjectsProps[] = [
   {
@@ -17,8 +23,18 @@ export const projects: ProjectsProps[] = [
     logo: RDFLogo,
     year: "2020-now",
   },
-  // {
-  //   name: "Animations",
-  //   link: "https://www.youtube.com/channel/UCQUbPhQlz0cQG7Km1xovJDg",
-  // },
+  {
+    name: "Inbox App",
+    link: "https://myinbox.vercel.app/",
+    banner: InboxBanner,
+    logo: InboxIcon,
+    year: "2020-now",
+  },
+  {
+    name: "Animations",
+    link: "https://www.youtube.com/channel/UCQUbPhQlz0cQG7Km1xovJDg",
+    banner: AnimationsBanner,
+    logo: AnimationsLogo,
+    year: "2014-2015",
+  },
 ];
